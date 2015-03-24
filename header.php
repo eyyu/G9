90bb1e408509811a40b44676cc9cfd6b2a4957e9
Trista Huang <trista.huang@outlook.com>
2015-03-24 14:01:09 -0700

    widened the content to 1000px, exported header & footer to external file

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+		<title>Hi F.I.V.E About</title>
+		<meta charset="utf-8">
+        <link rel="stylesheet" href="base.css">
+        <script type="text/javascript"  
+        src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js">
+        </script>
+        <script type="text/javascript" src="externalScript.js"></script>
+</head>
+<!--divide the body into 4 boxes-->
+
+<body>
+
+	<!--first box-->
+	<div id="header">
+		<div id="header-interior">
+            <img src="images/hi5cover.jpg" width="1000" height="402" alt="HeaderPhoto" usemap="#signin">
+               <map name="signin">
+                   <area shape="rect" coords="800,8,970,73" href="login.html" alt="login">
+               </map>
+		</div>
+		<div id="social-link">
+		</div>
+	</div>
+	
+	<!--second box-->
+	<div id="nav">
+            <ul id="navbar">    <!--changed to list form-->
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="stigma.html">Stigma</a></li>
+                <li><a href="toolkit.html">Tool Kit</a></li>
+                <li><a href="pledge.html">Pledge</a></li>
+                <li><a class="resources" href="resources.html">Resources</a></li>
+                
+            </ul>
+	</div>
