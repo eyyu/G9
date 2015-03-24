a03468cf0892f940af5812551bbc063a2d79f54e
Leia Fang <leiafang66@gmail.com>
2015-03-24 12:19:59 -0700

    sql file loaded

--- signup.php
+++ signup.php
@@ -1,18 +1,25 @@
 <?php
-    $fn = strtolower($_POST["firstname"]);
-    $ln = strtolower($_POST["lastname"]);
-    $email = strtolower($_POST["email"]);
- 
-    $format = $fn." ".$ln." ".$email;
+
+$dbhost = 'leiafang.webege.com';
+$dbuser = 'a3722077_leia';
+$dbpass = '7865380fh';
+$conn = mysql_connect($dbhost, $dbuser, $dbpass);
+if(! $conn )
+{
+  die('Could not connect: ' . mysql_error());
+}
+$sql = 'INSERT INTO a3722077_form '.
+       '(email, password) '.
+       'VALUES ( "leiafang66@gmail.com", 123456, NOW())';
+
+mysql_select_db('a3722077_form');
+$retval = mysql_query( $sql, $conn );
+if(! $retval )
+{
+  die('Could not enter data: ' . mysql_error());
+}
+echo "Entered data successfully\n";
+mysql_close($conn);
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <title></title>
-    </head>
-    <body>
-        
-    </body>
-</html>
